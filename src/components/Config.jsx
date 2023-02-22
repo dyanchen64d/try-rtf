dc6757a4cc142569b0d85eda4aa94dfdc6463255
chen.zhang <zhangchen64d@gmail.com>
2023-02-23 01:02:38 +0900

    update

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,14 +1,16 @@
 import { useTextureContext } from '../textureContext';
 
 const Config = () => {
-  const { changeTexture } = useTextureContext();
+  const { changeTexture, texture } = useTextureContext();
+
+  // console.log('texture', texture);
 
   return (
     <div className="config">
-      <button>
+      <button className={texture.name === 'cliff' ? '__active' : ''}>
         <div onClick={() => changeTexture('cliff')}>CLIFF</div>
       </button>
-      <button>
+      <button className={texture.name === 'wood' ? '__active' : ''}>
         <div onClick={() => changeTexture('wood')}>WOOD</div>
       </button>
     </div>
